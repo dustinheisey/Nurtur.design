4aef52cf019a8a7b5e21eb082b0733cae02903e9
Dustin Heisey <dustin@heisey.email>
2024-06-26 14:44:37 +0200

    chore: update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,30 @@
+import remarkGfm from 'remark-gfm'
+
 export default {
+  framework: {
+    name: '@storybook/web-components-vite',
+    options: {},
+  },
   stories: ['../src/**/*.mdx', '../**/*.story.js'],
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-links',
     '@storybook/addon-interactions',
-    '@storybook/addon-docs',
     'storybook-design-token',
     '@etchteam/storybook-addon-status',
     '@storybook/addon-themes',
     '@storybook/addon-viewport',
     'storybook-addon-pseudo-states',
+    '@chromatic-com/storybook',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
+      },
+    },
   ],
-  core: {
-    builder: '@storybook/builder-vite',
-  },
-  framework: {
-    name: '@storybook/web-components-vite',
-    options: {},
-  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -54,4 +54,5 @@ export default {
       },
     },
   },
+  tags: ['autodocs'],
 }
