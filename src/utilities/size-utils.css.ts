763f37ea3554edaa098666e6c858c31e9713bfd9
Dustin Heisey <dustin@heisey.email>
2024-11-14 15:05:27 +0100

    feat: utility styles

---
+++ src/utilities/size-utils.css.ts
@@ -0,0 +1,7 @@
+import { css } from 'lit';
+
+export const sizeUtilStyles = css`
+  .full {
+    inline-size: 100%;
+  }
+`;
---
+++ src/utilities/text-utils.css.ts
@@ -0,0 +1,19 @@
+import { css } from 'lit';
+
+export const textUtilStyles = css`
+  .text-start {
+    text-align: start !important;
+  }
+
+  .text-center {
+    text-align: center !important;
+  }
+
+  .text-end {
+    text-align: end !important;
+  }
+
+  .nowrap {
+    white-space: nowrap !important;
+  }
+`;
